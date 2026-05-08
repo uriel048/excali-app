43bc2f2bd93411ae85471d10971c99232edb7316
Uriel Ventura <urielventurakha@gmail.com>
2026-05-08 10:15:08 -0300

    Configura GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // MUITO IMPORTANTE: Garante que os caminhos sejam relativos
+  base: '/excali-app/',
 })
